07969b6dbbc0e5f0f269345ee4853ba46a64ee03
Benjamin Elliott <benjamin.elliott.be@gmail.com>
2023-07-31 11:29:55 -0400

    further changes

--- src/index.tsx
+++ src/index.tsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
         element: <Contact />
       },
       {
-        path: "/*",
+        path: "*",
         element: <Navigate to="/" />
       },
     ],
