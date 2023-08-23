c5eba865300fdad25005ac4b9778d556bc5ada6e
Benjamin Elliott <benjamin.elliott.be@gmail.com>
2023-08-23 16:03:33 -0400

    refactoring code, adjusting breakpoints

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -30,51 +30,49 @@ export const About = (props: any) => {
 
     return (
         <animated.section id="about" className="about" style={{...springs}}>
-            <article className='about-history'>
             <h2 className='section-title'>From coaching...</h2>
-                <div className="about-coaching">
-                    <div className='coaching-image'>
-                        <LazyLoadImage src={props.coachingImage} className="hero-image" alt="Ben & Colby" />
-                    </div>
-                    <div className='coaching-skills'>
-                        {
-                            props.coachingSkills.map((skill: {
-                                [x: string]: ReactNode; image: string | undefined; key: Key | null | undefined;}) => {
-                                return (
-                                    <div key={skill.key}>
-                                        <p className='skill-icon' >{skill.image}</p>
-                                        <p className='skill-text'>{skill.name}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+            <article className="about-coaching">
+                <div className='coaching-image'>
+                    <LazyLoadImage src={props.coachingImage} className="hero-image" alt="Ben & Colby" />
                 </div>
-                <h2 className='section-title'>...to coding.</h2>
-                <div className="about-coding">
                 <div className='coaching-skills'>
-                        {
-                            props.codingSkills.map((skill: {
-                                [x: string]: ReactNode; image: string | undefined; key: Key | null | undefined;}) => {
-                                return (
-                                    <div key={skill.key}>
-                                        <p className='skill-icon' >{skill.image}</p>
-                                        <p className='skill-text'>{skill.name}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className='coding-image'>
-                        <LazyLoadImage src={props.codingImage} className="hero-image" alt="Ben & Colby" />
-                    </div>
+                    {
+                        props.coachingSkills.map((skill: {
+                            [x: string]: ReactNode; image: string | undefined; key: Key | null | undefined;}) => {
+                            return (
+                                <div key={skill.key}>
+                                    <p className='skill-icon' >{skill.image}</p>
+                                    <p className='skill-text'>{skill.name}</p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-                <div className='about-benjamin'>
-                    <h2 className='section-title'>Soft skills meet hard work</h2>
-                    <p>As a Front-End wed developer, I've acquired all the skills required to build responsive, scalable pages and apps for a variety of uses. As a person, I've lived a life of challenges and learned the meaning of hard work, dedication and sacrifice. Every hat I wore along the way, has made me who I am today.</p>
+            </article>
+            <h2 className='section-title'>...to coding.</h2>
+            <article className="about-coding">
+                <div className='coaching-skills'>
+                    {
+                        props.codingSkills.map((skill: {
+                            [x: string]: ReactNode; image: string | undefined; key: Key | null | undefined;}) => {
+                            return (
+                                <div key={skill.key}>
+                                    <p className='skill-icon' >{skill.image}</p>
+                                    <p className='skill-text'>{skill.name}</p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-            
-            <div className='about-hobbies'>
+                <div className='coding-image'>
+                    <LazyLoadImage src={props.codingImage} className="hero-image" alt="Ben & Colby" />
+                </div>
+            </article>
+            <article className='about-benjamin'>
+                <h2 className='section-title'>Soft skills meet hard work</h2>
+                <p>As a Front-End wed developer, I've acquired all the skills required to build responsive, scalable pages and apps for a variety of uses. As a person, I've lived a life of challenges and learned the meaning of hard work, dedication and sacrifice. Every hat I wore along the way, has made me who I am today.</p>
+            </article>
+            <article className='about-hobbies'>
                 <h2 className="section-title">Hobbies</h2>
                 <div className='hobbies-wrapper'>
                     {
@@ -86,7 +84,6 @@ export const About = (props: any) => {
                         )
                     )}
                 </div>
-            </div>
             </article>
         </animated.section>
     );
