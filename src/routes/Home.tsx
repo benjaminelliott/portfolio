f7e63117d20253be3c9addf705e6eee27656f9dc
Benjamin Elliott <benjamin.elliott.be@gmail.com>
2023-10-06 10:51:44 -0400

    updated homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -16,12 +16,17 @@ export const Home = (props: any) => {
                 <div className="home-title-wrapper">
                     <h2 className="home-title"><strong className="home-title-strong">Mostly </strong>Front-End,</h2>
                     <h2 className="home-title"><strong className="home-title-strong">Sometimes </strong>Full-Stack,</h2>
-                    <h2 className="home-title"><strong className="home-title-strong">Always </strong><span className=" home-title-name">Benjamin</span></h2>
+                    <h2 className="home-title"><strong className="home-title-strong">Always </strong><span className=" home-title-name">Benjamin 👋🏽</span></h2>
                 </div>
                 <img className="hero-image" src={props.image} alt="whoops" />
             </article>
             <article className="home-lower">
-                <p className="text intro">I'm <strong className="benjamin strong">{props.name}</strong>. I have a passion for creating beautiful and practical things with <strong className="react strong">React</strong>. I'm always building, committing, learning, and continuing the grind.</p>
+                <div className="intro">
+                    <p className="text intro">I used my curiosity and creativity to creating this space to showcase my work and share my journey in the world of React development. In my corner of the web, you'll find a collection of projects that represent not only my technical skills but also my dedication to creating user-friendly and visually stunning web experiences. </p>
+                    <p className="text intro">From Front-End to Full-Stack applications, I'm constantly pushing the boundaries of what I can achieve and keeping up with the latest industry standards. I make daily commits to GitHub and I'm always working on something in the background. Stay tuned for the next stage in my coding evolution.</p>
+                    <p className="text intro"> So, I invite you to explore my work, read about my coding adventures, and get a glimpse into my coding style and philosophy. Whether you're looking to collaborate on a project, discuss a potential opportunity, or simply want to connect, I'm just a click away. 👊🏽</p>
+                </div>
+                
                 <div className="tech-stack">
                     <h4 className="section-title">Tech Stack</h4>
                     <div className="tech-logos">
