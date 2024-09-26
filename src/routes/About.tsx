57786a9f72807cf3c2e7afa5ce853f39990460aa
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-09-26 16:33:50 +0200

    removing import

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -3,7 +3,6 @@ import { PageHeader } from "../components/PageHeader";
 import { PageContent } from "../components/PageContent";
 import type { Skill, Hobby, AboutItem } from "../global";
 import { PageTitle } from "../components/PageTitle";
-import { AboutCard } from "../components/AboutCard";
 import { AboutList } from "../components/AboutList";
 
 export const About = ({
